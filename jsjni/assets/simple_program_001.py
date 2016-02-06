984c582b4b2816dbf138727bc9bcb146153d6eaf
weimingtom <weimingtom@qq.com>
2016-02-06 13:05:19 +0800

    jsjni

--- jsjni/assets/simple_program_001.py
+++
@@ -1,6 +0,0 @@
-#!C:/Python27/python.exe
-
-#see https://wiki.python.org/moin/SimplePrograms
-#1 line: Output
-
-print 'Hello, world!'
--- jsjni/assets/simple_program_002.py
+++
@@ -1,7 +0,0 @@
-#!C:/Python27/python.exe
-
-#see https://wiki.python.org/moin/SimplePrograms
-#2 lines: Input, assignment
-
-name = raw_input('What is your name?\n')
-print 'Hi, %s.' % name
